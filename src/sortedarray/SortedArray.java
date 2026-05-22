4a0196ef07bc6d0755c1848f7e242fbe44bd8da9
RenjiroKunio <renjiro.handoko@gmail.com>
2026-05-23 00:01:30 +0700

    Updated SortedArray (unfinished)

--- src/sortedarray/SortedArray.java
+++ src/sortedarray/SortedArray.java
@@ -1,5 +1,25 @@
 package sortedarray;
+import interfaces.AutocompleteStructure;
+import java.util.*;
 
-public class SortedArray {
+public class SortedArray implements AutocompleteStructure{
     
+    private final ArrayList<String> words;
+
+    public SortedArray () {
+        this.words = new ArrayList<>();
+    }
+
+    @Override
+    public void insert(String word) {
+        if (word == null || word.isEmpty()) return;
+
+        word = word.trim();
+
+        int index = Collections.binarySearch(words, word);
+
+        if (index < 0) {
+            words.add(-index - 1, word);
+        }
+    }
 }
